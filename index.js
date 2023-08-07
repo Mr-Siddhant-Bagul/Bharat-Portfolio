1e594b7b6ce5b31ac3d178a7a2764960e9d991d5
Mr-Siddhant-Bagul <siddhantbagul16@gmail.com>
2023-08-07 21:21:58 +0530

    Portfolio Website Complete

---
+++ index.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".Professions", {
+    strings : ["MERN Stack Developer", "Front-End Developer", "React Developer"],
+    typeSpeed: 75,
+    backSpeed: 75,
+    loop: true
+  })
